b78e40c1c2657eee493a009713642deb48bf62c4
SUJI0620 <aliceji98@naver.com>
2024-10-29 21:09:17 +0900

    Feat : Sticker

--- Assets/JSJ/Scripts/PicketSceneManager.cs
+++ Assets/JSJ/Scripts/PicketSceneManager.cs
@@ -6,20 +6,56 @@ using UnityEngine.UI;
 
 public class PicketSceneManager : MonoBehaviour
 {
+    public GameObject stickerPrefab;
+    public Canvas boardCanvas;
+    public Button btn_Sticker;
     public Button btn_GoToTown;
+
+    bool isActivate = false;
 
     void Start()
     {
+        btn_Sticker.onClick.AddListener(ActivateMakeSticker);
         btn_GoToTown.onClick.AddListener(GoToTown);
     }
 
     void Update()
     {
+        if (isActivate && Input.GetMouseButtonDown(0))
+        {
+            MakeSticker();
+        }
         
     }
+
 
     public void GoToTown()
     {
         SceneManager.LoadScene("Meta_Town_Scene");
+    }
+
+    public void ActivateMakeSticker()
+    {
+        isActivate = true;
+    }
+
+    public void MakeSticker()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(
+           boardCanvas.GetComponent<RectTransform>(),
+           mousePosition,
+           Camera.main, // 월드 공간에 사용할 카메라
+           out Vector3 worldPosition
+       );
+
+        GameObject sticker = Instantiate(stickerPrefab);
+        sticker.transform.SetParent(boardCanvas.transform, false);
+
+        Transform stickerPos = sticker.GetComponent<Transform>();
+        stickerPos.transform.position = mousePosition;
+
+
     }
 }
