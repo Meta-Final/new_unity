29bdc27bdb7668313686fe855217dbf8e5af654f
JSKIM-G <emflazlwm@naver.com>
2024-11-12 09:29:23 +0900

    1234

--- Assets/KJS/Script/AIBotSpawnner_KJS.cs
+++ Assets/KJS/Script/AIBotSpawnner_KJS.cs
@@ -1,32 +1,25 @@
 using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AIBotSpawnner_KJS : MonoBehaviourPunCallbacks
+public class AIBotSpawner_KJS : MonoBehaviourPunCallbacks
 {
-    private GameObject specialObjectInstance;
+    public string helperResourceName = "Helper"; // Resources 폴더에 있는 프리팹 이름
 
-    public override void OnJoinedRoom()
+    private void Start()
     {
-        // 방에 입장한 플레이어가 자신의 오브젝트를 생성하도록 합니다.
-        if (specialObjectInstance == null)
+        SpawnHelper(); // 씬이 시작될 때 Helper 프리팹 생성
+    }
+
+    private void SpawnHelper()
+    {
+        GameObject helperPrefab = (GameObject)Resources.Load(helperResourceName);
+        if (helperPrefab != null)
         {
-            SpawnSpecialObject();
+            PhotonNetwork.Instantiate(helperResourceName, Vector3.zero, Quaternion.identity); // Helper 프리팹을 네트워크 상에 생성
         }
-    }
-
-    private void SpawnSpecialObject()
-    {
-        // Resources 폴더에서 프리팹을 네트워크 상에 생성
-        Vector3 spawnPosition = GetSpawnPosition();
-        specialObjectInstance = PhotonNetwork.Instantiate("Helper", spawnPosition, Quaternion.identity, 0);
-    }
-
-    private Vector3 GetSpawnPosition()
-    {
-        // 원하는 위치에 오브젝트를 생성하기 위해 위치 설정 로직을 추가할 수 있습니다.
-        // 여기서는 예시로 랜덤 위치를 설정
-        return new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f));
+        else
+        {
+            Debug.LogWarning("Helper 프리팹을 Resources 폴더에서 찾을 수 없습니다.");
+        }
     }
 }
