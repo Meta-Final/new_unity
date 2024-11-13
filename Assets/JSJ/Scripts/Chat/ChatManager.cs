2ac230c3e5234405d4abd62166c89026d3c0dfb8
SUJI0620 <aliceji98@naver.com>
2024-11-13 18:35:34 +0900

    Feat : 전체 채팅, 닉네임 구현

--- Assets/JSJ/Scripts/Chat/ChatManager.cs
+++ Assets/JSJ/Scripts/Chat/ChatManager.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
+using Photon.Pun;
 
-public class ChatManager : MonoBehaviour
+public class ChatManager : MonoBehaviourPun
 {
     // Input Field
     public TMP_InputField inputChat;
@@ -20,9 +20,15 @@ public class ChatManager : MonoBehaviour
 
     // 채팅이 추가되기 전의 Content 의 H(높이) 값을 가지고 있는 변수
     float prevContentH;
+
+    // 닉네임 색상
+    Color nickNameColor;
     
     void Start()
     {
+        // 닉네임 색상을 랜덤하게 설정
+        nickNameColor = Random.ColorHSV();
+
         // inputChat 의 내용이 변경될 때 호출되는 함수 등록
         inputChat.onValueChanged.AddListener(OnValueChanged);
         // inputChat 엔터를 쳤을 때 호출되는 함수 등록
@@ -42,6 +48,22 @@ public class ChatManager : MonoBehaviour
         // 만약에 s 의 길이가 0 이면 함수를 나가자.
         if (s.Length == 0) return;
 
+        // 채팅 내용을 NickName : 채팅 내용
+        // "<collor=#ffffff> 원하는 내용 </color>"
+        string nick = "<color=#" + ColorUtility.ToHtmlStringRGB(nickNameColor) + ">" + PhotonNetwork.NickName + "</color>";
+        string chat = nick + " : " + s;
+
+        // AddChat RPC 함수 호출
+        photonView.RPC(nameof(AddChat), RpcTarget.All, chat);
+
+        // 강제로 inputChat 을 활성화 하자
+        inputChat.ActivateInputField();
+    }
+
+    // 채팅 추가 함수
+    [PunRPC]
+    void AddChat(string chat)
+    {
         // 새로운 채팅이 추가되기 전의 Content 의 H 값을 저장
         prevContentH = trContent.sizeDelta.y;
 
@@ -50,14 +72,11 @@ public class ChatManager : MonoBehaviour
         // ChatItem 컴포넌트 가져오자.
         ChatItem chatItem = go.GetComponent<ChatItem>();
         // 가져온 컴포넌트의 SetText 함수 실행
-        chatItem.SetText(s);
+        chatItem.SetText(chat);
         // 가져온 컴포넌트의 onAutoScroll 변수에 AutoScrollBottom 을 설정
         chatItem.onAutoScroll = AutoScrollBottom;
         // inputChat 에 있는 내용을 초기화
         inputChat.text = "";
-
-        // 강제로 inputChat 을 활성화 하자
-        inputChat.ActivateInputField();
     }
 
     // 채팅 추가 되었을 때 맨밑으로 Content 위치를 옮기는 함수
