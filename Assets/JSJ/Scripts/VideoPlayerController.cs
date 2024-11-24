5edb49a2587483c1fe0ff593e526fb430e2146cb
SUJI0620 <aliceji98@naver.com>
2024-11-24 22:34:38 +0900

    Fix : 비디오 문제 해결

--- Assets/JSJ/Scripts/VideoPlayerController.cs
+++ Assets/JSJ/Scripts/VideoPlayerController.cs
@@ -6,17 +6,18 @@ public class VideoPlayerController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     
-
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
 
+        // 경로 설정
         string videoPath = Path.Combine(Application.streamingAssetsPath, "Loginvideo.mp4");
+        
         videoPlayer.url = "file:///" + videoPath;
-
-        videoPlayer.Play();
-
         Debug.Log("Video Path: " + videoPlayer.url);
+
+        // 비디오 실행
+        videoPlayer.Play();
     }
 }
 
