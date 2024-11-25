65562680c7f48494d7d714c61bc25673d182561e
SeoHahyun <haqqm66@gmail.com>
2024-11-25 18:46:18 +0900

    edituidesigning

--- Assets/SHH/H_Script/Postit/H_NewFolder.cs
+++ Assets/SHH/H_Script/Postit/H_NewFolder.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class H_NewFolder : MonoBehaviour
 {
     public List<Texture> texs = new List<Texture>();
+    public GameObject contentsView;
+    public GameObject newFolder;
+    public Button Btn_close_mergeview;
 
     void Start()
     {
-        
+       
+        Btn_close_mergeview.onClick.AddListener(OnClickCloseContentview);
+
     }
 
     void Update()
     {
         
     }
+    private void OnClickContentView()
+    {
+        contentsView.SetActive(true);
+    }
+    private void OnClickCloseContentview()
+    {
+        contentsView.SetActive(false);
+    }
 }
+
