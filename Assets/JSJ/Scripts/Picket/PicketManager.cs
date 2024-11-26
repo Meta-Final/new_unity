1acaf15874ab503af0dbee007035f36d6b47d510
SUJI0620 <aliceji98@naver.com>
2024-11-27 01:06:30 +0900

    Feat : 피켓 수정

--- Assets/JSJ/Scripts/Picket/PicketManager.cs
+++ Assets/JSJ/Scripts/Picket/PicketManager.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 
 public class PicketManager : MonoBehaviourPun
 {
+    [Header("GameObject")]
     public GameObject player;
+    public GameObject nowMakePicket;   // 현재 생성된 Picket
+    public GameObject panelLinkNews;   // Picket이랑 기사 링크 여부 Panel
 
-    public GameObject panelLinkNews; // Picket이랑 기사 링크 여부 Panel
+    [Header("Button")]
+    public Button btn_Picket;   // Picket 생성 버튼
 
-    public Button btn_Picket; // Picket 생성 버튼
+    int picketIdCounter = 0;   // Picket ID를 부여하기 위한 카운터
 
     public PicketUIManager picketUIManager;
-
-    private int picketIdCounter = 0; // Picket ID를 부여하기 위한 카운터
-
-    public GameObject nowMakePicket;
 
     void Start()
     {
@@ -24,18 +24,21 @@ public class PicketManager : MonoBehaviourPun
 
         btn_Picket.onClick.AddListener(MakePicket);
 
+        // 기사 Panel 비활성화
         panelLinkNews.SetActive(false);
     }
 
-    void Update()
-    {
-    }
-
-    // 지정된 구역에서만 Picket 생성 가능
+    // Picket 생성 함수
     public void MakePicket()
     {
         StartCoroutine(CoMakePicket());
 
+        // 내 것이라면,
+        if (player.GetComponent<PhotonView>().IsMine)
+        {
+            // 기사 Panel 활성화
+            panelLinkNews.SetActive(true);
+        }
     }
 
    IEnumerator CoMakePicket()
@@ -48,13 +51,16 @@ public class PicketManager : MonoBehaviourPun
             // 'PicketZone' 에만 Picket 이 생성
             if (hit.collider.gameObject.layer == 17)
             {
+                // Picket 생성 위치
                 Vector3 picketPos = player.transform.position + player.transform.forward * 3f;
+                // Picket 생성 방향
                 Quaternion picketRot = Quaternion.LookRotation(player.transform.forward);
 
                 // Picket 생성
                 GameObject obj = PhotonNetwork.Instantiate("Picket", picketPos, picketRot);
 
                 yield return new WaitUntil(() => obj != null);
+
 
                 // Picket UI Manager에 현재 생성된 Picket 연결
                 picketUIManager.currentPicket = obj;
@@ -69,11 +75,6 @@ public class PicketManager : MonoBehaviourPun
                 }
 
                 picketIdCounter++; // ID 카운터 증가
-
-                if (player.GetComponent<PhotonView>().IsMine)
-                {
-                    panelLinkNews.SetActive(true);
-                }
             }
         }
     }
